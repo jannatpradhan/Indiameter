df06fc9e97ba7492601032d1cb3f3b4561fb5716
jannatpradhan <jannatpradhan777@gmail.com>
2020-04-27 18:22:13 -0700

    added last updated

--- indian_state/views.py
+++ indian_state/views.py
@@ -3,26 +3,14 @@ from django.http import JsonResponse
 from django.shortcuts import render,HttpResponse
 import requests
 
-"""
+
 req = requests.get('https://api.covid19india.org/data.json')
 data1 = req.json()
-s = data1['statewise']
+s = data1['statewise'][0]
+print(s['active'])
 
 
 
-req2 = requests.get('https://api.covid19india.org/v2/state_district_wise.json')
-data2 = req2.json()
-actualdata=[]
-for item in s:
-    code=item['statecode']
-
-    for o in data2:
-        if o['statecode']==code:
-            item.update({'district':o['districtData']})
-            actualdata.append(item)
-for item in actualdata:
-    print(item)
-"""
 
 
 
@@ -40,6 +28,7 @@ def home(request):
     req = requests.get('https://api.covid19india.org/data.json')
     data1 = req.json()
     s=data1['statewise']
+    allind=data1['statewise'][0]
     req2 = requests.get('https://api.covid19india.org/v2/state_district_wise.json')
     data2 = req2.json()
     actualdata = []
@@ -52,7 +41,7 @@ def home(request):
                 actualdata.append(item)
 
 
-    return render(request,'index.html',{'s':actualdata,'Total':Total,'new_case':new_case,'Total_death':Total_death})
+    return render(request,'index.html',{'allind':allind,'s':actualdata,'Total':Total,'new_case':new_case,'Total_death':Total_death})
 
 def check_symptom(request):
     return render(request,'check_symptom.html')
