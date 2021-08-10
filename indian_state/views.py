45550c67982e85c8a447d56994eadc4810e1eaab
[JannatPradhan-sys] <[Jannat-Kumar.Pradhan@spikewell.com]>
2021-08-10 19:09:30 +0530

    unknown commit

--- indian_state/views.py
+++ indian_state/views.py
@@ -1,6 +1,6 @@
 import json
-from django.http import JsonResponse
-from django.shortcuts import render,HttpResponse
+#from django.http import JsonResponse
+from django.shortcuts import render
 import requests
 
 
